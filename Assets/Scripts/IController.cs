52b537bcfd0ff9b15ad9a109175888924b33c0c4
Arkadii <arkadiy.delphi@gmail.com>
2022-05-27 20:00:54 +0300

    commit for testing git

--- Assets/Scripts/IController.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine.Events;
-
-public interface IController {
-    UnityEvent OnActing { get; set; }
-    
-}
--- Assets/Scripts/IMovable.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public interface IMovable {
-	
-	float Speed { get; set; }
-	Vector2 Velocity { get; set; }
-	
-	void Move();
-}
